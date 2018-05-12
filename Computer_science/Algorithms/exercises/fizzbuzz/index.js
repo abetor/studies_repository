ccb255d7a7caff446a048102c46252f6b56d9bfa
abetor <Molaytuh@gmail.com>
2018-05-12 12:19:49 +0500

    6 more classic exercises

--- Computer_science/Algorithms/exercises/fizzbuzz/index.js
+++ Computer_science/Algorithms/exercises/fizzbuzz/index.js
@@ -12,6 +12,19 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) 
+{
+    for (let i = 0; i < n; i++) {
+        if (i%15===0) {
+            console.log("fizzbuz");   
+        } else if (i%3===0) {
+            console.log("fizz");
+        } else if (i%5===0) {
+            console.log("buzz");
+        } else {
+            console.log(i);
+        }      
+    }
+}
 
 module.exports = fizzBuzz;
